7b53cab3d96e4ca96fc6a65feb5ccba8cab38742
sksarvesh007 <bt22csd002@iiitn.ac.in>
2024-01-03 21:45:03 +0530

    added finddistance function in module

--- handtrackingmodule.py
+++ handtrackingmodule.py
@@ -54,7 +54,17 @@ class handDetector():
             else :
                 fingers.append(0)
         return fingers
-
+    def finddistance(self , p1 , p2 , img , draw=True , r=15 , t=3):
+        x1 , y1 = self.lmlist[p1][1:]
+        x2 , y2 = self.lmlist[p2][1:]
+        cx , cy = (x1+x2)//2 , (y1+y2)//2
+        if draw :   
+            cv2.line(img , (x1,y1) , (x2,y2) , (255,0,255) , t)
+            cv2.circle(img , (x1,y1) , r , (255,0,255) , cv2.FILLED)
+            cv2.circle(img , (x2,y2) , r , (255,0,255) , cv2.FILLED)
+            cv2.circle(img , (cx,cy) , r , (255,0,255) , cv2.FILLED)
+        length = ((x2-x1)**2 + (y2-y1)**2)**0.5
+        return length , img , [x1,y1,x2,y2,cx,cy]
 def main():
     pTime=0
     cTime=0
